ae4b5804608e1b6386f2365271a9da73c5c2a6e3
Emmanuel778-tech <eemmanuelcastro12@gmail.com>
2026-03-06 15:45:59 +0000

    progress

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Script Started");
+
+function changeColor() {
+    console.log("click");
+    let color = prompt("Change the color for the poem");
+    let changeColor = document.getElementById("poem");
+    changeColor.style.color = color;
+}
+
+function changeFontSize() {
+    console.log("click");
+    let fontSize = promt("Change the font size");
+    let chnageFontSize = document.getElementById("poem");
+    chnageFontSize.style.fontSize;
+}
